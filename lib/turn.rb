a4e3f65d6c0ee8847e76f0294dc2ff82d465ed26
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-12 20:03:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,12 +9,21 @@ end
 
 #valid_move? makes sure the players input is good
 def valid_move? (board, position)
-  if (board[position] == 'X' || board[position] == 'O')
-    return false
-  elsif (position > board.length - 1)
-    return false
-  else
+  if ((board[position] != 'X' && board[position] != 'O') && (position >= 0 && position <= 8))
     return true
+  else
+    return false
   end
 end
- 
+
+#move let play take turn if their input is valid
+def move (board, user_input, token = "X")
+  if (valid_move?(board, user_input))
+    board[user_input] = token
+  else
+    until (valid_move?(board, user_input))
+      puts "Invalid Board Position try again with an integer between 1-9"
+      user_input = gets.strip
+    end
+  end
+end
